277f019b1a2486044bfb592058187c070d62f299
sob7an911-arch <sob7an911@gmail.com>
2026-06-05 15:54:24 +0300

    Implement do_HEAD method in DummyHandler
    Added do_HEAD method to handle HEAD requests.

--- bot.py
+++ bot.py
@@ -15,6 +15,12 @@ class DummyHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write("البوت يعمل بكفاءة!".encode('utf-8'))
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+
 def run_dummy_server():
     port = int(os.environ.get("PORT", 10000))
     server = HTTPServer(("0.0.0.0", port), DummyHandler)
